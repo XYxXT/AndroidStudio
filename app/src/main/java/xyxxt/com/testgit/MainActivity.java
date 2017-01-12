a825387dacb426f16940f55d9e06c6cb5effa9ad
XYxXT <qiangchen_995@hotmail.com>
2017-01-12 12:51:27 +0100

    Test1

--- app/src/main/java/xyxxt/com/testgit/MainActivity.java
+++ app/src/main/java/xyxxt/com/testgit/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        int test2;
         int test;
     }
 }
